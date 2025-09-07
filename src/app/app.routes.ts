f402bf59ee06890c84fd31601fe77280867ef30c
fx30651 <konrad.nowacki@rbinternational.com.pl>
2025-09-07 15:26:44 +0200

    Added working links in s3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'article1',
+    path: 'a1',
     loadComponent: () =>
       import('./../articles/kafka-part-1-basics/kafka-part-1-basics.component').then(
         (c) => c.KafkaPart1BasicsComponent
       ),
   },
   {
-    path: 'article2',
+    path: 'a2',
     loadComponent: () =>
       import('./../articles/kafka-part-2-basics/kafka-part-2-basics.component').then(
         (c) => c.KafkaPart2BasicsComponent
@@ -17,7 +17,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/contact',
+    redirectTo: '/a1',
     pathMatch: 'full',
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('blog');
 }
